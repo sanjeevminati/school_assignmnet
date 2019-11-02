3723d702507595265a287def87333b9f957e4422
Hackaman <43167327+sanjeevminati@users.noreply.github.com>
2019-11-02 08:54:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<conio.h>
+//#include<conio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<fstream>
@@ -7,10 +7,10 @@ using namespace std;
 
 struct student
 {
- char name[20];
- char reg[15];
- char course[10];
- float cgpa;
+	char name[20];
+	char reg[15];
+	char course[10];
+	int cgpa;
 };
 
 fstream file;
@@ -18,152 +18,121 @@ student obj;
 
 void add()
 {
- cout<<"Enter Name: ";
- cin>>obj.name;
- cout<<"Enter Registration Number: ";
- cin>>obj.reg;
- cout<<"Enter Course: ";
- cin>>obj.course;
- cout<<"Enter CGPA: ";
- cin>>obj.cgpa;
-
- file.open("database.txt",ios::app) ;
- file.write((char*)&obj,sizeof(obj));
- file.close();
+	cout<<"Enter Name: ";
+	cin>>obj.name;
+	cout<<"Enter Registration Number: ";
+	cin>>obj.reg;
+	cout<<"Enter Course: ";
+	cin>>obj.course;
+	cout<<"Enter CGPA: ";
+	cin>>obj.cgpa;
+	
+	file.open("database.txt",ios::app) ;
+	file.write((char*)&obj,sizeof(obj));
+	file.close();
 }
 
 void show_all()
 {     //  clrscr();
-	system("cls");
- file.open("database.txt",ios::in);
- file.read((char*)&obj,sizeof(obj));
-
- while (file.eof()==0)
- {
-  cout<<"Name: "<<obj.name<<endl;
-  cout<<"Registration Number: "<<obj.reg<<endl;
-  cout<<"Course: "<<obj.course<<endl;
-  cout<<"CGPA: "<<obj.cgpa<<endl<<endl;
-
-  file.read((char*)&obj,sizeof(obj));
- }
- file.close();
-
- getch();
+	system("clear");
+	file.open("database.txt",ios::in);
+	file.read((char*)&obj,sizeof(obj));
+	
+	while (file.eof()==0)
+	{
+		cout<<"Name: "<<obj.name<<endl;
+		cout<<"Registration Number: "<<obj.reg<<endl;
+		cout<<"Course: "<<obj.course<<endl;
+		cout<<"CGPA: "<<obj.cgpa<<endl<<endl;
+		
+		file.read((char*)&obj,sizeof(obj));
+	}
+	file.close();
+	
+	
 }
 
 void search()
 {      // clrscr();
- float user;
- cout<<"Enter CGPA: ";
- cin>>user;
- file.open("database.txt",ios::in);
- file.read((char*)&obj,sizeof(obj));
-
- while (file.eof()==0)
- {
-  if (obj.cgpa==user)
-  {
-   cout<<"Name: "<<obj.name<<endl;
-   cout<<"Registration Number: "<<obj.reg<<endl;
-   cout<<"Course: "<<obj.course<<endl;
-   cout<<"CGPA: "<<obj.cgpa<<endl<<endl;
-  }
-
-  file.read((char*)&obj,sizeof(obj));
- }
- file.close();
-
- getch();
+	//char user[20];
+	int user;
+	cout<<"Enter NAME ";
+	cin>>user;
+	file.open("database.txt",ios::in);
+	file.read((char*)&obj,sizeof(obj));
+	
+	while (file.eof()==0)
+	{
+		if (obj.cgpa == user)
+		{
+			cout<<"Name: "<<obj.name<<endl;
+			cout<<"Registration Number: "<<obj.reg<<endl;
+			cout<<"Course: "<<obj.course<<endl;
+			cout<<"CGPA: "<<obj.cgpa<<endl<<endl;
+		}
+		
+		file.read((char*)&obj,sizeof(obj));
+	}
+	file.close();
+	//system("clear");
+	
+	
 }
 
-void edit()
-{     //  clrscr();
-
- char user[15];
- cout<<"Enter registration Number: ";
- cin>>user;
-
- file.open("database.txt",ios::in|ios::out);
- file.read((char*)&obj,sizeof(obj));
-
- while (file.eof()==0)
- {
-  if (strcmp(obj.reg,user)==0)
-  {
-   cout<<"Name: "<<obj.name<<endl;
-   cout<<"Registration Number: "<<obj.reg<<endl;
-   cout<<"Course: "<<obj.course<<endl;
-   cout<<"CGPA: "<<obj.cgpa<<endl<<endl;
-
-   cout<<"\nEnter New course: ";
-   cin>>obj.course;
-
-   file.seekp(file.tellg()-sizeof(obj));
-   file.write((char*)&obj,sizeof(obj));
-   cout<<"\n\nFile Updated";
-   break;
-  }
-
-  file.read((char*)&obj,sizeof(obj));
- }
- file.close();
-
- getch();
-}
 
 
 int main()
 {
-// clrscr();
-
- //file.open("c:\database.txt",ios::out);
- //file.close();
-  int option;
- 
- string username,password;
- cout<<"\n --------- Admin Login --------\n";	
- cout<<"\n Enter username : ";
- cin>>username;
- cout<<"\n Enter password : ";
- cin>>password;
- if(username=="admin" && password=="admin@123") 
- {
-			 		
+	// clrscr();
+	
+	//file.open("c:\database.txt",ios::out);
+	//file.close();
+	int option;
+	
+	string username,password;
+	cout<<"\n --------- Admin Login --------\n";	
+	cout<<"\n Enter username : ";
+	cin>>username;
+	cout<<"\n Enter password : ";
+	cin>>password;
+	if(username=="admin" && password=="admin@123") 
+	{
+		
+		
+		while(1)
+		{
+			// clrscr();
 			
-			 while(1)
-			 {
-			 // clrscr();
-			 system("cls");
-			  cout<<"Enter 1 to Enter Record\n";
-			  cout<<"Enter 2 to Show All Record\n";
-			  cout<<"Enter 3 to Search Record\n";
-			  cout<<"Enter 4 to Exit\n";
-			  cout<<"\n\nEnter Option: ";
-			  cin>>option;
+			cout<<"Enter 1 to Enter Record\n";
+			cout<<"Enter 2 to Show All Record\n";
+			cout<<"Enter 3 to Search Record\n";
+			cout<<"Enter 4 to Exit\n";
+			cout<<"\n\nEnter Option: ";
+			cin>>option;
 			
-			  switch (option)
-			  {
-			   case 1:
-			    add();
-			    cout<<"\n\nRecord Entered\n";
-			    getch();
-			    break;
-			   case 2:
-			    show_all();
-			    break;
-			   case 3:
-			    search();
-			    break;
-			   case 4:
-			    exit(0);
-			  }
-			 }
-}
-else
-cout<<"\n Error ! Invalid Credintials..";	
-
- getch();
-
- return 0;
+			switch (option)
+			{
+				case 1:
+					add();
+					cout<<"\n\nRecord Entered\n";
+					
+					break;
+				case 2:
+					show_all();
+					break;
+				case 3:
+					system("clear");
+					search();
+					break;
+				case 4:
+					exit(0);
+			}
+		}
+	}
+	else
+		cout<<"\n Error ! Invalid Credintials..";	
+	
+	
+	
+	return 0;
 }
